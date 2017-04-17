568e1da64c1c0dddb0ea21acbe5aa1ac38668bf0
shangzhiyuan <shangzy@turingcat.com>
2017-04-17 16:38:32 +0800

    app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 var config = require('./config');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var mysql = require('mysql'); // node-mysql module
 var myConnection = require('express-mysql-connection');// express-mysql-connection middleware module
 var useDynamicRouter=['/api','/auth'];
 
 // default port where dev server listens for incoming traffic
 var port = config.port;
+
 var apiRouter = require('./routers/api');
 
 var app = express()
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(useDynamicRouter, myConnection(mysql, config.db.mysql, 'pool') );
 
