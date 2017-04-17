1a20d08330d3ac2a2e63998874e50abb8b14d81e
shangzhiyuan <shangzy@turingcat.com>
2017-04-17 16:38:16 +0800

    routers

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var apiRouter = express.Router();
 
-var mockData = require('../../mock-data.json');
+// @获取
+apiRouter.get('/todos', require('../api/get').todos);
+apiRouter.get('/tags', require('../api/get').tags);
 
-apiRouter.get('/todos/',function(req,res,next){
-  res.json(mockData.todos);
-});
-apiRouter.get('/tags/',function(req,res,next){
-  res.json(mockData.tags);
-});
+// @新增
+apiRouter.put('/todos', require('../api/put').todos);
+// apiRouter.put('/tags', require('../api/put').tags);
+
+// @修改
+//      整体修改
+apiRouter.post('/todos/', require('../api/post').todos);
+//      修改单独属性
+apiRouter.post('/todos/:attr', require('../api/post').todosAttr);
+
+// @删除
+apiRouter.delete('/todos/:todoID', require('../api/delete').todos);
+
+
+// apiRouter.get('/todos', require('../api/post').todos);
+// apiRouter.get('/tags', require('../api/post').tags);
 
 module.exports = apiRouter;
